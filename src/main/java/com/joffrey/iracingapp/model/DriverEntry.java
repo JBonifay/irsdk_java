bc1e1365cfe45c0b9a2ae41d06798743bfa3f2db
JBonifay <joffreybonifay83@gmail.com>
2020-07-10 10:59:00 +0200

    Refactoring: move class in model package

---
+++ src/main/java/com/joffrey/iracingapp/model/DriverEntry.java
@@ -0,0 +1,14 @@
+package com.joffrey.iracingapp;
+
+import lombok.Data;
+
+@Data
+public class DriverEntry {
+
+    private int carIdx;
+    private int carClassId;
+    private String driverName;
+    private String teamName;
+    private String carNumStr;
+
+}
