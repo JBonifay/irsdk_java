4bd6500012a17716c6b1683ed24e8d83fc1f42eb
JBonifay <joffreybonifay83@gmail.com>
2020-07-15 13:58:45 +0200

    Make Kernel32Impl/User32Impl package-private

--- src/main/java/com/joffrey/iracingapp/service/windows/User32Impl.java
+++ src/main/java/com/joffrey/iracingapp/service/windows/User32Impl.java
@@ -26,7 +26,7 @@ import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.win32.W32APIOptions;
 
-public interface User32Impl extends User32 {
+interface User32Impl extends User32 {
 
     static final User32Impl USER_32 = Native.loadLibrary("user32", User32Impl.class, W32APIOptions.DEFAULT_OPTIONS);
 
